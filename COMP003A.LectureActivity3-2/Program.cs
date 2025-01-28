1d239270ff61048a76588df2e77c406788aabd36
SalinasJason <jasonsalinas770@gmail.com>
2025-01-27 20:49:46 -0800

    Asked the user for a positive integer and added for loop

--- COMP003A.LectureActivity3-2/Program.cs
+++ COMP003A.LectureActivity3-2/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the iterative Statements Demo!\n");
+
+            // Prompt for a positive integer
+            int num;
+            Console.Write("Enter a positive integer to generate its multiplaction table: ");
+            num = int.Parse(Console.ReadLine());
+
+            // Generate multiplication table using a for loop
+            Console.WriteLine($"Multiplaction Table for {num}:");
+            // Loop to genrate the multiplication table
+            // the first statement initializes the loop variable i to 1
+            // the second statement specifies the condition for the loop to continue
+            // the third statement increments the loop variable  by 1
+            for (int i = 1; i <= 10; i++)
+            {
+                // display the multiplication table
+                Console.WriteLine($"{i} x {num} = {i * num}");
+            }
         }
     }
 }
